1cc33d98bbf0aca80cdae0164a9b70d71cbf82be
oseun <okseongeun4@gmail.com>
2022-05-11 03:22:07 +0900

    zustand persist 공부

--- src/store/navigation.js
+++ src/store/navigation.js
@@ -12,7 +12,15 @@ const useStoreNavigation = create(
         }),
         {
             name: 'navigation',
+            getStorage: () => sessionStorage,
+            // serialize: state => btoa(JSON.stringify(state.isType)),
+
+            // serialize: state => btoa(JSON.stringify(state.isType)),
+            // deserialize: partialize => atob(JSON.parse(partialize)),
             partialize: state => ({ isType: state.isType }),
+
+            // partialize: state => ({ isType: btoa(JSON.stringify(state.isType)) }),
+            // deserialize: isType => atob(JSON.parse(isType)),
         },
     ),
 );
