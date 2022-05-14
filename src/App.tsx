df6f1f3e7da074fee426c9922e0843fa67ee8283
oseun <okseongeun4@gmail.com>
2022-05-14 18:17:52 +0900

    figlet 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import figlet from 'figlet';
+import standard from 'figlet/importable-fonts/Standard.js';
 import IndexRouter from './route';
 import './assets/reset.css';
 
 function App() {
+    useEffect(() => {
+        figlet.parseFont('Standard', standard);
+
+        figlet.text(
+            'OoKiiii',
+            {
+                font: 'Standard',
+            },
+            function (err, data) {
+                console.log(data);
+            },
+        );
+    }, []);
+
     return (
         <div className="App">
             <IndexRouter />
